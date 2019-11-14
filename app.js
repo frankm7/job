df41ca73f8cd1a372be11067bda0067213f16ae3
Frank <ezechibueze07@gmail.com>
2019-11-14 15:53:15 +0100

    2nd commit

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const PORT = 5000;
 //middleware
 app.use('/assert', express.static(path.resolve(__dirname, 'public')));
 
+// view folder is where our template engine files will sit
+app.set('views', path.resolve(__dirname, 'view'))
+//setting the template to pug
+app.set('view engine', 'pug')
 
 //route
 app.get('/', function (req, res)
@@ -20,8 +24,10 @@ app.get('/person/:name', function (req, res)
 {
     console.log(req.params);
     console.log(req.query)
-    return res.send(`<h1>Hi, ${req.params.name}</h1>`);
+    return res.render('person',
+        {name: req.params.name, });
 });
+
 //to make your server go live on the port
 app.listen(PORT, function(){
     console.log(`server running on po
